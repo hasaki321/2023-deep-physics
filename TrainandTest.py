ce8e1960cbeb94ba661bd6353d5e5c9b8fd46887
QingLzu <139302806+QingLzu@users.noreply.github.com>
2023-09-19 12:19:05 +0800

    Add files via upload

--- TrainandTest.py
+++ TrainandTest.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
-import torch.nn as nn
+import panda as pd
 import matplotlib.pyplot as plt
-
+from preprocess import dataset
+from sklearn.preprocessing import MinMaxScaler,StandardScaler
 def loss_fn(criterion,alpha,inputs,outputs,labels):
     
     #let the length of output determin which loss to return 
@@ -15,8 +16,37 @@ def loss_fn(criterion,alpha,inputs,outputs,labels):
     #alse return mse
     return loss,mse
     
+def pre_training(model,criterion,alpha,optimizer,epoch,lr_scheduler,para_path,device,flag):
+    if flag == 5 :
+        scaler = MinMaxScaler()
+        pre_traindata = pd.read_excel('./data/test 3.xlsx',engine="openpyxl")
+        pre_traindata = np.array(pre_traindata)
+        pre_traindata = dataset(pre_traindata,'even','>126',5, scaler)
 
-def train_MLP(train_loader,model,criterion,alpha,optimizer,epoch,fold,lr_scheduler,para_path,device):
+        best = 100
+        for idx in range(epoch):
+            for i, (inputs, labels) in enumerate(pre_traindata):
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                optimizer.zero_grad()
+                outputs = model(inputs)
+
+                # change into a loss function
+                loss, _ = loss_fn(criterion, alpha, inputs, outputs, labels)
+
+                if loss.item() < best and idx > epoch * 0.95:
+                    best = loss.item()
+                    torch.save(model.state_dict(), f'{para_path}/model.ckpt')
+                    print(f"loss:{loss.item()} saving...")
+
+            loss.backward()
+            optimizer.step()
+
+        lr_scheduler.step()
+
+    model.load_state_dict(torch.load(f'{para_path}/model.ckpt'))
+
+def train_MLP(train_loader,model,criterion,alpha,optimizer,epoch,fold,lr_scheduler,para_path,device,flag):
     losses = []
     best = 100
     for idx in range(epoch):
@@ -29,9 +59,9 @@ def train_MLP(train_loader,model,criterion,alpha,optimizer,epoch,fold,lr_schedul
             #change into a loss function
             loss,_ = loss_fn(criterion,alpha,inputs,outputs,labels)
             
-            if loss.item()<best and idx>epoch*0.98:
+            if loss.item()<best and idx>epoch*0.95:
                 best = loss.item()
-                torch.save(model.state_dict(), f'{para_path}/model_fold_{fold + 1}.ckpt')
+                torch.save(model.state_dict(), f'{para_path}/model_{flag + 1}_fold_{fold + 1}.ckpt')
                 print(f"loss:{loss.item()} saving...")
 
         loss.backward()
@@ -40,8 +70,8 @@ def train_MLP(train_loader,model,criterion,alpha,optimizer,epoch,fold,lr_schedul
     losses.append(loss.item())
     lr_scheduler.step()
 
-def test_MLP(test_loader,model,fold,criterion,alpha,plot,para_path,device):
-    model.load_state_dict(torch.load(f'{para_path}/model_fold_{fold + 1}.ckpt'))
+def test_MLP(test_loader,model,fold,criterion,alpha,plot,para_path,device,flag):
+    model.load_state_dict(torch.load(f'{para_path}/model_{flag + 1}_fold_{fold + 1}.ckpt'))
     model.eval()
     test_mses,test_losses,test_output,test = [],[],[],[]
     
@@ -57,13 +87,14 @@ def test_MLP(test_loader,model,fold,criterion,alpha,plot,para_path,device):
             #separate loss and mse here
             test_mses.append(mse.item())
             test_losses.append(loss.item())
-        rmse = sum(test_mses)**0.5
-        print(f'Test Loss: {sum(test_losses)/len(test_losses):.5f}, Test RMSE: {rmse:.5f}') 
+        total_mse = sum(test_mses)/len(test_losses)
+        rmse = total_mse**0.5
+        print(f'Test Loss: {total_mse:.5f}, Test RMSE: {rmse:.5f}')
     if plot:
         test = np.array(torch.stack(test).cpu()).reshape(-1)
         test_output = np.array(torch.stack(test_output).cpu()).reshape(-1)
         plt.scatter(test,test)
         plt.scatter(test,test_output)
         plt.show()
-    return rmse
+    return total_mse,rmse
    
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ def get_models(model,input_size,L1,L2):
     if model=='FM_MLP':
         return FM_MLP(input_size,L1)
     elif model == 'AE_MLP':
-        return AE_MLP(input_size,16,L1,L2)
+        return AE_MLP(input_size,32,L1,L2)
 
 class FM_MLP(nn.Module):
     def __init__(self, input_size,Linear_size):
