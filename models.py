37a8d12ee4e98987efd30cecec26d2c1e38875dc
QingLzu <139302806+QingLzu@users.noreply.github.com>
2023-10-20 19:14:21 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -1,60 +1,46 @@
 import torch.nn as nn
 import torch
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
-def get_models(model,input_size,L1,L2):
-    if model=='FM_MLP':
-        return FM_MLP(input_size,L1)
+def get_models(model,input_size,epsilon):
+    if model=='SVR':
+        SVR_model = make_pipeline(StandardScaler(), SVR(kernel='rbf', C=100, gamma=0.01, epsilon=epsilon))
+        return SVR_model
     elif model == 'AE_MLP':
-        return AE_MLP(input_size,32,L1,L2)
-
-class FM_MLP(nn.Module):
-    def __init__(self, input_size,Linear_size):
-        super(FM_MLP, self).__init__()
-        self.input_size = input_size
-
-        self.fnn1 = nn.Sequential(
-            nn.Linear(input_size,Linear_size),
-            nn.ELU(),
-            nn.BatchNorm1d(Linear_size),
-            nn.Linear(Linear_size,1),
-        )
-
-    def forward(self, x):
-        out = self.fnn1(x)
-        return out
+        return AE_MLP(input_size)
 
 class AE_MLP(nn.Module):
-    def __init__(self, input_size,hid_feature,Linear_size1,Linear_size2):
+    def __init__(self, input_size):
         super().__init__()
-        self.input_size = input_size
-        self.hid_feature = hid_feature
 
         self.encoder = nn.Sequential(
-            nn.Linear(input_size,Linear_size1),
+            nn.Linear(input_size,8),
             nn.ELU(),
-            nn.BatchNorm1d(Linear_size1),
-            nn.Linear(Linear_size1,Linear_size2),
+            nn.BatchNorm1d(8),
+            nn.Linear(8,16),
             nn.ELU(),
-            nn.Linear(Linear_size2,hid_feature),
+            nn.Linear(16,32),
         )
         
         self.decoder = nn.Sequential(
-            nn.Linear(hid_feature,Linear_size2),
+            nn.Linear(32,16),
             nn.ReLU(),
-            nn.BatchNorm1d(Linear_size2),
-            nn.Linear(Linear_size2,Linear_size1),
+            nn.BatchNorm1d(16),
+            nn.Linear(16,8),
             nn.ELU(),
-            nn.Linear(Linear_size1,input_size),
+            nn.Linear(8,input_size),
         )
         
         self.predicter = nn.Sequential(
-            nn.Linear(hid_feature,Linear_size2),
+            nn.Linear(32,16),
             nn.ReLU(),
-            nn.BatchNorm1d(Linear_size2),
-            nn.Linear(Linear_size2,Linear_size1),
+            nn.BatchNorm1d(16),
+            nn.Linear(16,8),
             nn.ELU(),
-            nn.BatchNorm1d(Linear_size1),
-            nn.Linear(Linear_size1,1),
+            nn.BatchNorm1d(8),
+            nn.Linear(8,1),
         )
     
     def forward(self, x):
@@ -62,4 +48,5 @@ class AE_MLP(nn.Module):
         out = self.decoder(hid)
         y = self.predicter(hid)
         return out,y
-    
+        
+
